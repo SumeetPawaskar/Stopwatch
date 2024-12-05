29aa6124cc3632550a89a685c9753e9508d746af
SumeetCodeJC <code.sumeet9@gmail.com>
2024-12-05 14:49:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+
+  
+  const toggleTimer = () => {
+    if (isRunning) {
+      clearInterval(intervalId);
+      setIsRunning(false);
+    } else {
+      const newIntervalId = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+      setIntervalId(newIntervalId);
+      setIsRunning(true);
+    }
+  };
+
+
+  const resetTimer = () => {
+    clearInterval(intervalId);
+    setIsRunning(false);
+    setSeconds(0);
+  };
+
+  
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="stopwatch-container">
+        <h1>Stopwatch</h1>
+        <div className="time-display">
+          <span>{formatTime(seconds)}</span>
+        </div>
+        <div className="button-container">
+          <button onClick={toggleTimer}>
+            {isRunning ? 'Stop' : 'Start'}
+          </button>
+          <button onClick={resetTimer}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
