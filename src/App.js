4e71a2d0a9fbf3b978437b5455148848c5baec94
SumeetCodeJC <code.sumeet9@gmail.com>
2024-12-05 15:08:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
-  const [seconds, setSeconds] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  const [seconds, setSeconds] = useState(0); 
+  const [isRunning, setIsRunning] = useState(false); 
   const [intervalId, setIntervalId] = useState(null);
 
   
   const toggleTimer = () => {
     if (isRunning) {
-      clearInterval(intervalId);
+      clearInterval(intervalId); 
       setIsRunning(false);
     } else {
       const newIntervalId = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds + 1);
+        setSeconds((prevSeconds) => prevSeconds + 1); 
       }, 1000);
       setIntervalId(newIntervalId);
       setIsRunning(true);
     }
   };
 
-
+  
   const resetTimer = () => {
-    clearInterval(intervalId);
+    clearInterval(intervalId); 
     setIsRunning(false);
-    setSeconds(0);
+    setSeconds(0); 
+  };
+
+ 
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60); 
+    const remainingSeconds = seconds % 60; 
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`; // Format as MM:SS
   };
 
   
-  const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-  };
-
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId); 
+    };
+  }, [intervalId]); 
   return (
     <div className="App">
       <div className="stopwatch-container">
